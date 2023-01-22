f41f81d00f2abeefc6bf0350e6230309b37a6973
Vanessa Machado Silva <lampetrolaboratorioufu@gmail.com>
2023-01-22 15:20:45 -0300

    teste

--- TrabalhoFinalPoo2-main/test/model/AdapterAluguelTest.java
+++ TrabalhoFinalPoo2-main/test/model/AdapterAluguelTest.java
@@ -109,46 +109,35 @@ public class AdapterAluguelTest {
       
     }
 
-    /**
-     * Test of getId method, of class AdapterAluguel.
-     */
-    /*@Test
+   
+    @Test
     public void testGetId() {
         System.out.println("getId");
         AdapterAluguel instance = new AdapterAluguel();
         UUID expResult = null;
         UUID result = instance.getId();
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+   
     }
 
-    /**
-     * Test of getNome method, of class AdapterAluguel.
-     */
-    /*@Test
+   
+    @Test
     public void testGetNome() {
         System.out.println("getNome");
         AdapterAluguel instance = new AdapterAluguel();
         String expResult = "";
         String result = instance.getNome();
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
 
-    /**
-     * Test of getCpf method, of class AdapterAluguel.
-     */
-    /*@Test
+   
+    @Test
     public void testGetCpf() {
         System.out.println("getCpf");
         AdapterAluguel instance = new AdapterAluguel();
         int expResult = 0;
         int result = instance.getCpf();
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
 
     /**
